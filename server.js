dea2e9b715734f9ddace3a293bbf2633e5ffdd5a
ihimanshubhutani <bhutanihimanshu98@gmail.com>
2020-05-19 21:46:45 +0530

    creating skeleton of chat app

--- server.js
+++ server.js
@@ -1,45 +1,23 @@
- 
-let app = require('express')();
+const io = require('socket.io')(3000)
 
-let http = require('http').Server(app);
+const users = {}
+const connection=[];
 
-let io = require('socket.io')(http);
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
-
-io.on('connection', (socket) => {
-    io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
-
-
-    socket.on('disconnect', () => {
-        console.log('disconnected')
-        io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
-    })
+io.sockets.on('connection', socket => {
+    connection.push(socket);
+  
+  socket.on('new-user',data => {
+    socket.join(data.room);
+    users[socket.id] = data.name;
+    io.sockets.to(data.room).emit('user-connected', data.name)
+  })
 
 
-
-    socket.on('chat-message', (msg) => {
-        socket.broadcast.emit('chat-message', msg)
-    })
-    socket.on('joined', (name) => {
-        socket.broadcast.emit('joined', name)
-    })
-    socket.on('leaved', (name) => {
-        socket.broadcast.emit('leaved', name)
-    })
-
-    socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', data)
-    })
-    socket.on('stoptyping', () => {
-        socket.broadcast.emit('stoptyping')
-    })
-
-
-})
-
-http.listen(3000, () => {
-    console.log('Server is started at http://localhost:3000')
+  socket.on('send-chat-message', data => {
+    io.sockets.to(data.room).emit('chat-message', { message: data.message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 })
